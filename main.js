59c9a408f8a5c8d6241a5b171cee65e53ce705ce
TatsukiFukushima <tatsuki2222net@gmail.com>
2023-02-06 12:58:10 +0900

    画面幅の変更に自動で対応

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
+let beforeWindowHeight;
+let beforeWindowWidth;
 let r = 0;
 let heightWithoutBar = 0;
 const allArc = 300;
 let lengthArc = allArc;
 let size = 0;
-let barWeight = 0
+let barHeight = 0
 let rand = Math.floor(Math.random()*100);
 let lastTouched = 0;
 
 function setup() {
   if (windowHeight > windowWidth) {
-      barWeight = windowWidth*0.08;
-      heightWithoutBar = windowHeight - barWeight;
+      barHeight = windowWidth*0.08;
+      heightWithoutBar = windowHeight - barHeight;
       r = windowWidth*0.87;
   } else {
-      barWeight = windowHeight*0.06;
-      heightWithoutBar = windowHeight - barWeight;
+      barHeight = windowHeight*0.06;
+      heightWithoutBar = windowHeight - barHeight;
       r = heightWithoutBar*0.87;
   }
-  size = r*0.4;
+  size = r*0.5;
 
   createCanvas(windowWidth, windowHeight);
 }
@@ -28,23 +30,31 @@ function draw() {
     reset();
   }
 
+  // 画面サイズが変わった場合
+  if (beforeWindowHeight != windowHeight || beforeWindowWidth != windowWidth) {
+    setup();
+    beforeWindowHeight = windowHeight;
+    beforeWindowWidth = windowWidth;
+  }
+
   // 画面リセット
   noStroke();
-  fill(30);
+  fill(20);
   rect(0, 0, windowWidth, windowHeight);
 
   // 中央の描画
-  fill(100);
+  fill(60);
   arc(windowWidth*0.5, heightWithoutBar*0.5, r, r, (1.5 - 2*(lengthArc/allArc))*PI, 1.5*PI);
-  fill(30);
+  fill(20);
   ellipse(windowWidth*0.5, heightWithoutBar*0.5, r*0.85, r*0.85);
   fill("#5ab966");
   textSize(size);
+  textFont('Helvetica');
   textAlign(CENTER, CENTER);
   text(rand, windowWidth*0.5, heightWithoutBar*0.5);
 
   // 下部の描画
-  fill(100);
+  fill(60);
   rect(0, heightWithoutBar, windowWidth, windowHeight);
   fill("#5ab966");
   rect(0, heightWithoutBar, windowWidth*(rand/100), windowHeight);
